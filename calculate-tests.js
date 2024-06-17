5e7ece5142628f70e76d308a8990d3ab513632a3
alexandrucristian15 <87300455+alexandrucristian15@users.noreply.github.com>
2024-06-18 01:15:39 +0300

    Update calculate-tests.js

--- calculate-tests.js
+++ calculate-tests.js
@@ -1,18 +1,19 @@
 const fs = require('fs');
 const csv = require('csv-parser');
 
-const csvFilePath = 'cypress/fixtures/links.csv'; // Actualizează calea completă către fișierul links.csv
+const csvFilePath = 'fixtures/links.csv'; // Actualizează calea relativă către fișierul links.csv
 
 let numTests = 0;
 
 fs.createReadStream(csvFilePath)
-    .pipe(csv())
-    .on('data', (row) => {
-        numTests++;
-    })
-    .on('end', () => {
-        console.log(`Număr de teste necesare: ${numTests}`);
-    })
-    .on('error', (err) => {
-        console.error('Eroare la citirea fișierului CSV:', err);
-    });
+  .pipe(csv())
+  .on('data', (row) => {
+    numTests++;
+  })
+  .on('end', () => {
+    console.log(numTests); // Returnează doar numărul de teste necesare
+  })
+  .on('error', (err) => {
+    console.error('Eroare la citirea fișierului CSV:', err);
+    process.exit(1); // Ieși cu cod de eroare în caz de eroare
+  });
