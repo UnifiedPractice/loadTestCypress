b5a1e78594129e730a70471dd8c50a8f94f7a182
Alex <alexandru.virbanescu@omnisourcetech.com>
2024-06-18 01:08:28 +0300

    LOAD TEST DEVELOPMENT

---
+++ calculate-tests.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const csv = require('csv-parser');
+
+const csvFilePath = 'C:/Users/Alex/Desktop/loadTests/loadTestCypress/cypress/fixtures/links.csv'; // Actualizează calea completă către fișierul links.csv
+
+let numTests = 0;
+
+fs.createReadStream(csvFilePath)
+    .pipe(csv())
+    .on('data', (row) => {
+        numTests++;
+    })
+    .on('end', () => {
+        console.log(`Număr de teste necesare: ${numTests}`);
+    })
+    .on('error', (err) => {
+        console.error('Eroare la citirea fișierului CSV:', err);
+    });
